7730e00914c6c881ae8dc22206a3cd6d4ccdc2ed
Paula Díaz <pauladesireediazsuarez@alumno.ieselrincon.es>
2026-02-23 19:26:40 +0000

    Intento de formulario

--- js/script.js
+++ js/script.js
@@ -18,3 +18,68 @@ function moverCarrusel(){
 }
 
 moverCarrusel();
+
+const abrir = document.getElementById("abrirFormulario");
+const modal = document.getElementById("modal");
+const cerrar = document.getElementById("cerrarModal");
+
+abrir.addEventListener("click", function(e){
+        e.preventDefault();
+        modal.style.display = "block";
+});
+
+cerrar.addEventListener("click", function(){
+        modal.style.display = "none";
+});
+
+window.addEventListener("click", function(e){
+    if(e.target === modal){
+        modal.style.display = "none";
+    }
+});
+
+const otroCheck = document.getElementById("otroCheck");
+const otroGenero = document.getElementById("otroGenero");
+
+otroCheck.addEventListener("change", function(){
+    if(this.checked){
+        otroGenero.style.display = "block";
+        otroGenero.required = true;
+    } else {
+        otroGenero.style.display = "none";
+        otroGenero.required = false;
+    }
+});
+
+const generoFavorito = document.getElementById("generoFavorito");
+const otroFavorito = document.getElementById("otroFavorito");
+
+generoFavorito.addEventListener("change", function(){
+
+    if(this.value === "otro"){
+        otroFavorito.style.display = "block";
+        otroFavorito.required = true;
+    } else {
+        otroFavorito.style.display = "none";
+        otroFavorito.required = false;
+    }
+});
+
+
+const formulario = document.getElementById("formulario");
+
+formulario.addEventListener("submit", function(e){
+    const checkboxes = document.querySelectorAll(".checkbox-group input[type='checkbox']");
+    let algunoMarcado = false;
+
+    checkboxes.forEach(function(checkbox){
+        if(checkbox.checked){
+            algunoMarcado = true;
+        }
+    });
+
+    if(!algunoMarcado){
+        e.preventDefault();
+        alert("Debes seleccionar al menos un género");
+    }
+});
